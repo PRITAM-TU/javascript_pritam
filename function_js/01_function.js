30e624ee13ecd8901d5b454c6e22103297ae7253
Pritam Tung <pritamtung03@gmail.com>
2025-06-13 07:20:44 +0000

    add file

---
+++ function_js/01_function.js
@@ -0,0 +1,18 @@
+function oddNumber(a,b)
+{
+   return a+b;
+
+}
+console.log(oddNumber(10,39));
+let a=20;
+let b=30;
+let c;
+{
+    c=a+b;
+}
+console.log(c);
+const cahi= ()=>{
+    return `pritam is not good ${"boy"}`;
+
+}
+cahi();
