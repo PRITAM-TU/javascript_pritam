93dd6a5db5388c49c939542bc5d3e3c02f4496ee
Pritam Tung <pritamtung03@gmail.com>
2025-06-13 06:29:35 +0000

    add file

---
+++ api_js/object_api.js
@@ -0,0 +1,12 @@
+const object_1={
+    name:"pritan_tung",
+    roll_number:17600123062,
+    email: "pritamgoogle@gmail.com",
+    age:21,
+    location:"jaiopur",
+ 
+
+}
+console.log(Object.keys(object_1));
+const {location:loc}=object_1
+console.log(loc);
