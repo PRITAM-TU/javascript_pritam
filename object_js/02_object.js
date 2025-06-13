9d417bc116aa661ae91b374f12a6cf690577dd68
Pritam Tung <pritamtung03@gmail.com>
2025-06-13 06:15:07 +0000

    add file

---
+++ object_js/02_object.js
@@ -0,0 +1,20 @@
+const object={
+    name:"pritan_tung",
+    roll_number:17600123062,
+    email: "pritamgoogle@gmail.com",
+    age:21,
+    location:"jaiopur",
+  
+
+}
+const object_1={
+    name:"pritan_tung",
+    roll_number:17600123062,
+    email: "pritamgoogle@gmail.com",
+    age:21,
+    location:"jaiopur",
+  
+
+}
+object.logemail="login";
+console.log(object);
